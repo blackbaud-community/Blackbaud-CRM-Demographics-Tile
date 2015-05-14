5ca138f3a1fdda0870bd7a716a4d38cc09b66b3a
blackbaud-johnly <John.Lyons@blackbaud.com>
2015-05-14 15:40:48 -0400

    Add Visual Studio project and other repo files to new tile repo

---
+++ Blackbaud.CustomFx.DemographicsTile/Blackbaud.CustomFx.DemographicsTile.UIModel/ConstituentSummaryDemographicsTileViewFormUIModel.vb
@@ -0,0 +1,7 @@
+Public Class ConstituentSummaryDemographicsTileViewFormUIModel
+
+    Private Sub ConstituentSummaryDemographicsTileViewFormUIModel_Loaded(ByVal sender As Object, ByVal e As Blackbaud.AppFx.UIModeling.Core.LoadedEventArgs) Handles Me.Loaded
+
+    End Sub
+
+End Class
