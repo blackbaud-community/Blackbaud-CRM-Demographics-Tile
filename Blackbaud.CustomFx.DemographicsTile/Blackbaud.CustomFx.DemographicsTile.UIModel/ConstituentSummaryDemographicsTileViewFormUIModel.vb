b102c70ca021a08533a6e2e1d8c5f76d6c8fb255
blackbaud-johnly <John.Lyons@blackbaud.com>
2015-05-18 14:40:04 -0400

    add logic to vb file to update page after add/edit actions

--- Blackbaud.CustomFx.DemographicsTile/Blackbaud.CustomFx.DemographicsTile.UIModel/ConstituentSummaryDemographicsTileViewFormUIModel.vb
+++ Blackbaud.CustomFx.DemographicsTile/Blackbaud.CustomFx.DemographicsTile.UIModel/ConstituentSummaryDemographicsTileViewFormUIModel.vb
@@ -4,4 +4,19 @@ Public Class ConstituentSummaryDemographicsTileViewFormUIModel
 
     End Sub
 
+    Private Sub _editdemogrpahics_DataFormSaved(sender As Object, e As UIModeling.Core.DataFormSavedEventArgs) Handles _editdemographics.DataFormSaved
+        'Navigate to Personal Info tab, Constituent Page.
+        Me.NavigateToPage(New Guid("88159265-2b7e-4c7b-82a2-119d01ecd40f"), New Guid("04a78290-9651-4aed-88d3-a179972eafe8"), MyBase.RecordId)
+    End Sub
+
+    Private Sub _editpersonalinfo_DataFormSaved(sender As Object, e As UIModeling.Core.DataFormSavedEventArgs) Handles _editpersonalinfo.DataFormSaved
+        'Navigate to Personal Info tab, Constituent Page.
+        Me.NavigateToPage(New Guid("88159265-2b7e-4c7b-82a2-119d01ecd40f"), New Guid("04a78290-9651-4aed-88d3-a179972eafe8"), MyBase.RecordId)
+    End Sub
+
+    Private Sub _addalias_DataFormSaved(sender As Object, e As UIModeling.Core.DataFormSavedEventArgs) Handles _addalias.DataFormSaved
+        'Navigate to Personal Info tab, Constituent Page.
+        Me.NavigateToPage(New Guid("88159265-2b7e-4c7b-82a2-119d01ecd40f"), New Guid("04a78290-9651-4aed-88d3-a179972eafe8"), MyBase.RecordId)
+    End Sub
+
 End Class
